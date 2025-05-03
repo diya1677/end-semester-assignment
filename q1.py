ff50446ebf833233714ed76f4c54afc854129c81
diya1677 <diyakathait13@gmail.com>
2025-05-03 17:37:39 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,7 @@
+#make a zero error division error handler with built in exceptions
+def divide_numbers(a,b):
+    try:
+        result=a/b
+        print("result=",result)
+    except Zero Division Error:
+        print("error:cannot divide by zero")    
